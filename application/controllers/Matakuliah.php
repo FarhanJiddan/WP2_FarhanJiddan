7ba6dad0e3697a3e235126f652f03e7a3a8edba8
FarhanJiddan <hansjiddan66@gmail.com>
2021-10-13 07:06:17 +0700

    Mengubah controller Matakuliah.php sesuai dengan yang ada di Pertemuan 5 (Membuat Form Validasi)

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -31,7 +31,6 @@ class Matakuliah extends CI_Controller
                 'nama' => $this->input->post('nama'),
                 'sks' => $this->input->post('sks')
             ];
-
             $this->load->view('view-data-matakuliah', $data);
         }
     }
